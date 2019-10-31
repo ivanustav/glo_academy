86a0830e80f41abfa5b7fd8cfc611a307df8f71b
Ivan Ustavitckiy <gita21@mail.ru>
2019-10-31 10:05:48 +0300

    g

--- lesson07/lesson07task2/scrypt.js
+++ lesson07/lesson07task2/scrypt.js
@@ -1,13 +1,47 @@
-let submit = document.getElementById('start'),
-    plusInc = document.querySelector('.btn_plus .income_add'),
-    plusExp = document.querySelector('.btn_plus .expenses_add'),
-    checkBox = document.querySelector('#deposit-check'),
-    incomeItem = document.querySelectorAll('.additional_income-item'),
-    incomeAmount = document.querySelector('.income-amount'),
-    salary = document.querySelector('.salary-amount'),
-    incomeTitle = document.querySelector('.income-title'),
-    addIncomeTitle = document.querySelector('.additional_income-item'),
-    expensesTitle = document.querySelector('.expenses-title'),
-    expensesAmount = document.querySelector('.expenses-amount'),
-    addExpensesAmount = document.querySelector('.additional_expenses-item'),
-    targetAmount = document.querySelector('.target-amount');
+'use strict';
+
+let body = document.querySelector('body'),
+    books = document.querySelectorAll('.books'),
+    book = document.querySelectorAll('.book'),
+    advertising = document.querySelector('.adv'),
+    target = document.querySelectorAll('a'),
+
+    ul = document.querySelectorAll('ul'), //список книг
+    li = document.querySelectorAll('li'), //его элементы
+
+    chapter8 = document.createElement('li'); //добавление элемента для главы 8
+
+
+body.setAttribute('style', 'background-image: url(./image/you-dont-know-js.jpg)'); // задаем новый фон
+
+books[0].insertBefore(book[1], book[0]);
+books[0].insertBefore(book[4], book[2]);
+books[0].insertBefore(book[3], book[2]);
+books[0].insertBefore(book[5], book[2]); 
+
+advertising.remove('adv');
+
+
+
+target[4].textContent ='"Книга 3. this и Прототипы Объектов"';
+
+
+
+ul[2].appendChild(chapter8);  
+chapter8.textContent = 'Глава 8: За пределами ES6'; 
+ 
+
+
+console.log(ul[2]);
+console.log(li);
+
+
+
+ul[5].insertBefore(li[55], li[48]);
+ul[5].insertBefore(li[48], li[52]);
+ul[0].insertBefore(li[6], li[4]);
+ul[0].insertBefore(li[8], li[4]);
+ul[0].insertBefore(li[2], li[10]);
+ul[2].insertBefore(li[3], li[5]);
+
+
